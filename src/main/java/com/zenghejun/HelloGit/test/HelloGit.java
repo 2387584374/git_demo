c0ccf1c8e86e50f769ebd51dbd50bf341a3462be
Zenghejun <2387584274@qq.com>
2021-11-30 19:17:02 +0800

    second commit

---
+++ src/main/java/com/zenghejun/HelloGit/test/HelloGit.java
@@ -0,0 +1,15 @@
+package com.zenghejun.HelloGit.test;
+
+public class HelloGit {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello Git!");
+        System.out.println("Hello Git!");
+        System.out.println("Hello Git!");
+        System.out.println("Hello Git!");
+        System.out.println("Hello Git!");
+        System.out.println("Hello Git!");
+    }
+}
